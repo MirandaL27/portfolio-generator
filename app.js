b71ee1c78389a7e90e975f11b364eb9229a89b41
Cadenza <mirandalucsb@gmail.com>
2021-12-21 11:53:11 -0700

    first use of inquirer

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-// const printProfileData = profileDataArr => {
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-//   };
-  
-//   printProfileData(profileDataArgs);
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
 
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
